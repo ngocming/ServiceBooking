a23598aafda8c2ea127cc7645d4f78ca5db2e554
ngoc <ngochoa4009@gmail.com>
2026-09-17 23:51:36 +0700

    add guard

--- ServiceBooking.Client/src/app/app.routes.ts
+++ ServiceBooking.Client/src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { Bookings } from './pages/bookings/bookings';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
-
+import { authGuard } from './guards/auth-guard';
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: 'bookings', component: Bookings },
+  { path: 'bookings', component: Bookings, canActivate: [authGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-];
+];
---
+++ ServiceBooking.Client/src/app/guards/auth-guard.ts
@@ -0,0 +1,12 @@
+import { CanActivateFn,Router  } from '@angular/router';
+import { inject } from '@angular/core';
+export const authGuard: CanActivateFn = (route, state) => {
+  const token = localStorage.getItem('token');
+  const router = inject(Router);
+  if (token) {
+    return true;
+  }
+  
+  return router.createUrlTree(['/login']);
+
+};
